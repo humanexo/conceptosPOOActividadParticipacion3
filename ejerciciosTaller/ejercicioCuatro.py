6167b3ace406618dcb53cf194076ac9b9f1f00ed
Andrés Felipe <andresdediaz10@gmail.com>
2023-02-17 10:36:38 -0500

    Completing exercises

--- ejerciciosTaller/ejercicioCuatro.py
+++ ejerciciosTaller/ejercicioCuatro.py
@@ -1,10 +1,31 @@
 # 4. Cree una clase Rectángulo la cual contiene dos atributos de instancia que representan los puntos que definen sus esquinas.
 # Agregue métodos a la clase Rectángulo para calcular su perímetro, calcular su área e indicar si el rectángulo es un cuadrado.
 
+from ejercicioDos import Punto
+
 
 class Rectangulo:
-    def __init__(self, x: int, y: int):
-        self.x: int = x
-        self.y: int = y
+    def __init__(self, punto_1: Punto, punto_2: Punto):
 
+        self.punto_1 = punto_1
+        self.punto_2 = punto_2
 
+        self.base = self.punto_2.x - self.punto_1.x
+        self.altura = self.punto_2.y - self.punto_1.y
+
+    def calcular_perimetro(self) -> float:
+
+        perimetro = 2 * self.base + 2 * self.altura
+        return perimetro
+
+    def calcular_area(self) -> float:
+
+        area = self.base * self.altura
+        return area
+
+    def si_es_cuadrado(self):
+
+        if self.base == self.altura:
+            print("Si es un cuadrado")
+        else:
+            print("No es un cuadrado")
--- ejerciciosTaller/ejercicioDos.py
+++ ejerciciosTaller/ejercicioDos.py
@@ -5,20 +5,24 @@ class Punto:
         self.x: int = x
         self.y: int = y
 
+
+    # 3. A la clase del punto anterior, defínale los siguientes métodos:
+    # - Un método mostrar que imprima por consola las coordenadas del punto
+    # - Un método mover que cambie las coordenadas del punto
+    # - Un método calcular_distancia que calcule la distancia de la instancia actual con otro punto.
+
+
     def mostrarPunto(self):
         print(f"{self.x}, X, {self.y} Y")
 
-
-    def moverpunto(self, nuevaX : int, nuevaY : int ):
+    def moverpunto(self, nuevaX: int, nuevaY: int):
         self.x = nuevaX
         self.y = nuevaY
 
     def calculardistancia(self, otropunto):
-        distanciaenx = self.x - otropunto.x
-        distanciaeny = self.y - otropunto.y
+        distanciaenx = self.x-otropunto.x
+        distanciaeny =self.y-otropunto.y
+        print(distanciaenx, distanciaeny)
 
-
-    def __init__(self, coordenadasPunto):
-        self.coordenadas_del_Punto: Punto = coordenadasPunto
 
 
