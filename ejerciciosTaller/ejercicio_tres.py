31e0c0bee705afd32057e8368a28688a14a13b96
Andrés Felipe <andresdediaz10@gmail.com>
2023-02-17 10:37:05 -0500

    Completing exercises

--- ejerciciosTaller/ejercicio_tres.py
+++ ejerciciosTaller/ejercicio_tres.py
@@ -1,10 +1,5 @@
 
 
-# 3. A la clase del punto anterior, defínale los siguientes métodos:
-# - Un método mostrar que imprima por consola las coordenadas del punto
-# - Un método mover que cambie las coordenadas del punto
-# - Un método calcular_distancia que calcule la distancia de la instancia actual con otro punto.
-
 if __name__ == "__main__":
     from ejercicioDos import Punto
 
@@ -14,9 +9,9 @@ if __name__ == "__main__":
     Punto1.moverpunto(6,9)
 
 
-
     Punto2 = Punto(11,5)
     Punto2.mostrarPunto()
+
     Punto2.calculardistancia(Punto1)
 
 
